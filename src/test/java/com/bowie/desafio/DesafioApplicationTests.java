9dedc0bc3611b3acceb7a941af27b1bce4690310
Bowi3 <leonnardofender@gmail.com>
2024-09-25 16:04:25 -0300

    Initial commit

---
+++ src/test/java/com/bowie/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bowie.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
